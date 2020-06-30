884f1eeb8818cfed78da1c7de0187ad4e135838c
Yusuff Faruq <faruqyusuff437@gmail.com>
2020-06-30 12:00:31 +0100

    initial commit

---
+++ controllers/auth_controller.go
@@ -0,0 +1,13 @@
+package controllers
+
+func RegsiterWithEmailAndPassword() {} //jwt
+
+func LoginWithEmailAndPassword() {} //jwt
+
+func Logout() {} //jwt
+
+func LoginWithGoogle() {}
+
+func LoginWithFacebook() {}
+
+func LoginWithGithub() {}
